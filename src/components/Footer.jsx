8343e2618bdcfe79a51173b5120765ee3178e7ff
Vaibhav <sidanavaibhav@gmail.com>
2024-11-07 00:33:13 +0530

    text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 function Footer(){
     return <div className="flex font-poppins flex-row wrap text-white justify-between mt-28">
-        <div className="basis-1/4 ml-8 flex flex-col justify-between h-20">
+        <div className="basis-1/4 ml-8 flex flex-col justify-between text-sm h-20">
             <div>Get in contact</div>
             <div>mail: xyz@gmail.com </div>
             <div> phone: +91 9999999999</div>
             
         </div>
-        <div className="basis-1/4  flex flex-col justify-between h-20" >
+        <div className="basis-1/4  flex flex-col justify-between text-sm mr-4 md:mr-0 h-20" >
             <a>Terms and conditions</a>
             <a>Refund policy </a>
             <a>About us</a>
