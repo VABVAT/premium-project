ab9957785f3929f60be1598b7238eb743b4dc0b5
VABVAT <136883802+VABVAT@users.noreply.github.com>
2024-11-10 03:57:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes, Outlet, Link } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
-import ProductCatalog from './components/productCatalog.jsx';
+import ProductCatalog from './components/ProductCatalog.jsx';
 
 function App() {
   return (
